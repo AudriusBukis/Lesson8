388c90be65160724bf5f66f3f6e11e93097baea3
AudriusBukis <audriusbukis@gmail.com>
2022-01-18 15:28:44 +0200

    First program upload

---
+++ astuntaPaskaita/astuntaPaskaita/Structures/Animals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace astuntaPaskaita
+{
+    public struct Animals
+    {
+        public string AnimalName { get; private set; }
+        public string AnimalSpecies { get; private set; }
+
+        public Animals(string animalName, string animalSpecies)
+        {
+            AnimalName = animalName;
+            AnimalSpecies = animalSpecies;
+        }
+    }
+}
---
+++ astuntaPaskaita/astuntaPaskaita/Structures/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace astuntaPaskaita
+{
+    public struct Point
+    {
+        public double CoordineteX { get; private set; }
+        public double CoordineteY { get; private set; }
+
+        public Point(double coordineteX, double coordineteY)
+        {
+            CoordineteX = coordineteX;
+            CoordineteY = coordineteY;
+        }
+    }
+}
